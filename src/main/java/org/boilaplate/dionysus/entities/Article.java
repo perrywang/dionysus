695bd8541b832b5d832120393d4e5c94f6d3f2f4
Lu, Jun <luj1985@gmail.com>
2015-02-05 20:50:45 +0800

    move configurations into META-INF folder

--- src/main/java/org/boilaplate/dionysus/entities/Article.java
+++ src/main/java/org/boilaplate/dionysus/entities/Article.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.Table;
@@ -15,8 +16,7 @@ public class Article implements Serializable {
 
 	private static final long serialVersionUID = 4106093798545531113L;
 
-	@Id
-	@GeneratedValue
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name="id")
 	private Long id;
 	
--- src/main/java/org/boilaplate/dionysus/entities/Category.java
+++ src/main/java/org/boilaplate/dionysus/entities/Category.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,8 +15,7 @@ public class Category implements Serializable {
 
 	private static final long serialVersionUID = 2384283567572219724L;
 
-	@Id
-	@GeneratedValue
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name="id")
 	private Long id;
 	
