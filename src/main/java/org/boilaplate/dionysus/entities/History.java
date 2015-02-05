2247fe58a38d4f47f8c36671de831b8ee80984ed
Lu, Jun <luj1985@gmail.com>
2015-02-05 21:26:04 +0800

    add log4j configurations

--- src/main/java/org/boilaplate/dionysus/entities/History.java
+++ src/main/java/org/boilaplate/dionysus/entities/History.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,8 +15,7 @@ public class History implements Serializable {
 
 	private static final long serialVersionUID = -3558156230583594674L;
 
-	@Id
-	@GeneratedValue
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id")
 	private Long id;
 }
--- src/main/java/org/boilaplate/dionysus/entities/Reply.java
+++ src/main/java/org/boilaplate/dionysus/entities/Reply.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -15,8 +16,7 @@ public class Reply implements Serializable {
 
 	private static final long serialVersionUID = 173375477700239586L;
 
-	@Id
-	@GeneratedValue
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id")
 	private Long id;
 
