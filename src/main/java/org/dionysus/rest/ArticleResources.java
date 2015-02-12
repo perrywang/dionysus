e8b1a4468a03d5bc8571e9a75ebbe010107fcef8
Lu, Jun <luj1985@gmail.com>
2015-02-13 03:24:03 +0800

    add restful example

--- src/main/java/org/dionysus/rest/ArticleResources.java
+++ src/main/java/org/dionysus/rest/ArticleResources.java
@@ -5,19 +5,31 @@ import java.util.List;
 import org.dionysus.model.Article;
 import org.dionysus.repositories.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/articles")
 public class ArticleResources {
-	
+
 	@Autowired
 	private ArticleRepository service;
 
-	@RequestMapping(method= RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Article> getArticle() {
 		return service.findAll();
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public @ResponseBody Article addArticle(@RequestBody Article article) {
+		return service.save(article);
+	}
+
+	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+	public @ResponseBody Article updateArticle(@RequestBody Article article) {
+		return service.save(article);
 	}
 }
--- src/main/webapp/assets/app/dionysus.js
+++ src/main/webapp/assets/app/dionysus.js
@@ -1,25 +1,28 @@
-
-ArticleModel = Backbone.Model.extend({});
-ArticleCollection = Backbone.Collection.extend({
-	model : ArticleModel
-});
-
-ArticleView = Marionette.ItemView.extend({
-  template: _.template("<span><%= name %></span>"),
-  tagName: 'li'
-});
-
-ArticleCollectionView = Marionette.CollectionView.extend({
-  tagName: "ul",
-  childView: ArticleView
-});
-
 var Dionysus = new Marionette.Application();
 
 Dionysus.addRegions({
 	headerRegion : 'header',
 	mainRegion : 'main',
 	footerRegion : 'footer'
+});
+
+ArticleModel = Backbone.Model.extend({});
+ArticleCollection = Backbone.Collection.extend({
+	model : ArticleModel,
+	url: '/api/v1/articles'
+});
+
+ArticleView = Marionette.ItemView.extend({
+  template: _.template("<span><%= title %></span>"),
+  tagName: 'li'
+});
+
+ArticleCollectionView = Marionette.CollectionView.extend({
+  tagName: "ul",
+  childView: ArticleView,
+  collectionEvents: {
+    "sync": "render"
+  }
 });
 
 Dionysus.addInitializer(function(options){
@@ -29,12 +32,13 @@ Dionysus.addInitializer(function(options){
   Dionysus.mainRegion.show(articlesView);
 });
 
-var articles = new ArticleCollection([
-  { name: 'Test 1' },
-  { name: 'Test 2' },
-  { name: 'Test 3' }
-]);
+var articles = new ArticleCollection();
                                    
 $(document).ready(function() {
 	Dionysus.start({ articles : articles });
+	articles.fetch().fail(function() {
+		console.log('fetch articles failed');
+	}).done(function() {
+		console.log('fetch success');
+	});
 });
