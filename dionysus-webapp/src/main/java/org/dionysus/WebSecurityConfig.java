5824c72cbf3884b3e0a3079dd55856a1278e955d
Lu, Jun <luj1985@gmail.com>
2015-02-14 17:38:06 +0800

    add login form

--- dionysus-webapp/src/main/java/org/dionysus/WebSecurityConfig.java
+++ dionysus-webapp/src/main/java/org/dionysus/WebSecurityConfig.java
@@ -23,17 +23,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 	    http.csrf().disable()
 	    	.authorizeRequests()
-	    	.antMatchers(HttpMethod.POST, "/api/v1/*").hasAnyRole("USER", "ADMIN")
-	    	.antMatchers(HttpMethod.PUT, "/api/v1/*").hasAnyRole("USER", "ADMIN")
-	    	.antMatchers(HttpMethod.DELETE, "/api/v1/*").hasAnyRole("USER", "ADMIN")
+	    	.antMatchers("/api/v1/*").hasAnyRole("USER", "ADMIN")
 	    	.anyRequest()
-	    	.authenticated();	
+	    	.authenticated()
+	    .and()
+	    	.formLogin()
+	    	.permitAll()
+	    .and()
+	    	.logout()
+            .permitAll();
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder registry) throws Exception {
-		registry.userDetailsService(securityUserService);
-	}
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder registry) throws Exception {
+//		registry.userDetailsService(securityUserService);
+//	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
