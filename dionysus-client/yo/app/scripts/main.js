5fcfff3d82300a032d607ac39343c2842f240ae0
Lu, Jun <luj1985@gmail.com>
2015-02-26 22:47:11 +0800

    init Marionette

--- dionysus-client/yo/app/scripts/main.js
+++ dionysus-client/yo/app/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('\'Allo \'Allo!');
+var Dionysus = new Marionette.Application();
+
+Dionysus.addRegions({
+  headerRegion: 'header',
+  mainRegion: 'main',
+  footerRegion: 'footer'
+});
+
+Dionysus.HeaderView = Marionette.ItemView.extend({
+  template: '#header-template'
+});
+
+Dionysus.MainView = Marionette.ItemView.extend({
+  template: '#main-template'
+});
+
+Dionysus.FooterView = Marionette.ItemView.extend({
+  template: '#footer-template'
+});
+
+Dionysus.addInitializer(function() {
+  var header = new Dionysus.HeaderView(),
+      main   = new Dionysus.MainView(),
+      footer = new Dionysus.FooterView();
+  Dionysus.headerRegion.show(header);
+  Dionysus.mainRegion.show(main);
+  Dionysus.footerRegion.show(footer);
+});
+
+Dionysus.start();
+
