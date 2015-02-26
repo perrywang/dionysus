21669414aef62c3fa813749fb9dcf3543faa4f38
Lu, Jun <luj1985@gmail.com>
2015-02-26 23:20:44 +0800

    enable LESS configuration

--- dionysus-client/yo/Gruntfile.js
+++ dionysus-client/yo/Gruntfile.js
@@ -50,6 +50,10 @@ module.exports = function (grunt) {
       gruntfile: {
         files: ['Gruntfile.js']
       },
+      less: {
+        files: ['<%= config.app %>/styles/{,*/}*.less'],
+        tasks: ['less:server', 'autoprefixer']
+      },
       styles: {
         files: ['<%= config.app %>/styles/{,*/}*.css'],
         tasks: ['newer:copy:styles', 'autoprefixer']
@@ -155,6 +159,39 @@ module.exports = function (grunt) {
       }
     },
 
+    less: {
+      options: {
+        paths: ['./bower_components'],
+      },
+      dist: {
+        options: {
+          cleancss: true,
+          report: 'gzip'
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/styles',
+          src: '*.less',
+          dest: '.tmp/styles',
+          ext: '.css'
+        }]
+      },
+      server: {
+        options: {
+          sourceMap: true,
+          sourceMapBasepath: '<%= config.app %>/',
+          sourceMapRootpath: '../'
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/styles',
+          src: '*.less',
+          dest: '.tmp/styles',
+          ext: '.css'
+        }]
+      }
+    },
+
     // Add vendor prefixed styles
     autoprefixer: {
       options: {
@@ -175,6 +212,10 @@ module.exports = function (grunt) {
       app: {
         ignorePath: /^\/|\.\.\//,
         src: ['<%= config.app %>/index.html']
+      },
+      less: {
+        src: ['<%= config.app %>/styles/{,*/}*.less'],
+        ignorePath: /(\.\.\/){1,2}bower_components\//
       }
     },
 
@@ -340,12 +381,14 @@ module.exports = function (grunt) {
     // Run some tasks in parallel to speed up build process
     concurrent: {
       server: [
+        'less:server',
         'copy:styles'
       ],
       test: [
         'copy:styles'
       ],
       dist: [
+        'less:dist',
         'copy:styles',
         'imagemin',
         'svgmin'
