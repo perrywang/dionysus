ccc36ebb9c24aa44d5893f9a752e87b5734263b0
Lu, Jun <luj1985@gmail.com>
2015-02-27 12:26:00 +0800

    enable spring security

--- dionysus-webapp/src/main/java/org/dionysus/WebSecurityConfig.java
+++ dionysus-webapp/src/main/java/org/dionysus/WebSecurityConfig.java
@@ -4,56 +4,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	@Qualifier("dionysus")
 	private UserDetailsService securityUserService;
-
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		    .withUser("admin").password("admin").roles("ADMIN", "USER").and()
+			.withUser("user").password("password").roles("USER");
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//	    http.csrf().disable()
-//	    	.antMatcher("/api/v1/*")
-//	    		.authorizeRequests()
-//	    		.antMatchers("/*").hasAnyRole("USER", "ADMIN")
-//	    		.anyRequest()
-//	    		.authenticated();
-	    http.csrf().disable()
-	    	.antMatcher("/api/v1")
-	    	.authorizeRequests()
-	    	.antMatchers("/*")
-	    	.anonymous();
-//	    .and()
-//	    	.formLogin()
-//	    	.permitAll()
-//	    .and()
-//	    	.logout()
-//            .permitAll();
+		http.antMatcher("/api/v1/**")
+			.authorizeRequests()
+				.anyRequest().hasRole("USER")
+				.and()
+			.httpBasic();
 	}
-
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/");
-	}
-
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		    .withUser("admin")
-		    .password("admin")
-		    .roles("ADMIN", "USER")
-		.and()
-			.withUser("user")
-			.password("password")
-			.roles("USER");
-//		auth.userDetailsService(securityUserService);
+		web.ignoring()
+			.antMatchers("/")
+			.antMatchers("/font/**")
+			.antMatchers("/styles/**")
+			.antMatchers("/images/**")
+			.antMatchers("/scripts/**");
 	}
 }
