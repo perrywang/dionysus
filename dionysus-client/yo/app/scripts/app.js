a7a084c2018bfb35b52a2c5cecc44dcea060a1ac
Lu, Jun <luj1985@gmail.com>
2015-02-27 15:07:16 +0800

    enable database reconnect

--- dionysus-client/yo/app/scripts/app.js
+++ dionysus-client/yo/app/scripts/app.js
@@ -1,10 +1,16 @@
-var Dionysus = new Marionette.Application();
-
 Dionysus.addRegions({
   headerRegion: 'header',
   mainRegion: 'main',
   footerRegion: 'footer'
 });
 
+Dionysus.addInitializer(function() {
+  var header = new Dionysus.HeaderView(),
+      main   = new Dionysus.MainView(),
+      footer = new Dionysus.FooterView();
+  Dionysus.headerRegion.show(header);
+  Dionysus.mainRegion.show(main);
+  Dionysus.footerRegion.show(footer);
+});
 
-
+Dionysus.start();
---
+++ dionysus-client/yo/app/scripts/root.js
@@ -0,0 +1,1 @@
+var Dionysus = new Marionette.Application();
