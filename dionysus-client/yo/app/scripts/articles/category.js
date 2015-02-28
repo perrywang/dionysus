1b862a2891921ade82c9d9f385213cada26c556b
Lu, Jun <luj1985@gmail.com>
2015-02-28 21:28:29 +0800

    add category model

--- dionysus-client/yo/app/scripts/articles/category.js
+++ dionysus-client/yo/app/scripts/articles/category.js
@@ -1,1 +1,12 @@
-Dionysus.Category = Backbone.Model.extend({});
+Dionysus.Category = Backbone.Model.extend({
+  urlRoot: "api/v1/categories"
+});
+
+Dionysus.CategoryCollection = Backbone.Collection.extend({
+  url: "api/v1/categories",
+  parse: function(response) {
+    return response._embedded.categories;
+  },
+  model: Dionysus.Category
+});
+
