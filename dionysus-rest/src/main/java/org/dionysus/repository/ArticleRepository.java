2fa96b00397a51977cd91d18f532fa719c9f95c7
Lu, Jun <luj1985@gmail.com>
2015-03-06 00:15:39 +0800

    add jpa data search

--- dionysus-rest/src/main/java/org/dionysus/repository/ArticleRepository.java
+++ dionysus-rest/src/main/java/org/dionysus/repository/ArticleRepository.java
@@ -1,10 +1,15 @@
 package org.dionysus.repository;
 
+import java.util.List;
+
 import org.dionysus.domain.Article;
+import org.dionysus.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "articles", path = "articles")
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
+	List<Article> findByCategory(@Param("category") Category category);
 }
