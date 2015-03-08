e3231b05bc13a7e883612badbc9680983ec3976d
Lu, Jun <luj1985@gmail.com>
2015-03-08 17:18:25 +0800

    add light admin configuration

--- dionysus-webapp/src/main/java/org/dionysus/WebApplication.java
+++ dionysus-webapp/src/main/java/org/dionysus/WebApplication.java
@@ -3,21 +3,24 @@ package org.dionysus;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
-public class WebApplication extends SpringBootServletInitializer {
-
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class WebApplication extends SpringBootServletInitializer {	
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.profiles("prod").sources(DomainApplicationContext.class);
+		return application.profiles("prod").sources(WebApplication.class, LightAdminWebApplication.class);
 	}
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder()
 			.profiles("dev")
-			.sources(DomainApplicationContext.class)
+			.sources(WebApplication.class, LightAdminWebApplication.class)
 			.run(args);
 	}
 }
