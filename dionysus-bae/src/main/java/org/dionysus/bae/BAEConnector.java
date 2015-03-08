7740306502dfc6deb1805fbbf840445ecbab9420
Lu, Jun <luj1985@gmail.com>
2015-03-09 00:21:58 +0800

    build bae application info

--- dionysus-bae/src/main/java/org/dionysus/bae/BAEConnector.java
+++ dionysus-bae/src/main/java/org/dionysus/bae/BAEConnector.java
@@ -3,29 +3,36 @@ package org.dionysus.bae;
 import java.util.List;
 
 import org.springframework.cloud.AbstractCloudConnector;
+import org.springframework.cloud.CloudException;
 import org.springframework.cloud.FallbackServiceInfoCreator;
 import org.springframework.cloud.app.ApplicationInstanceInfo;
 import org.springframework.cloud.service.UriBasedServiceData;
 import org.springframework.cloud.util.EnvironmentAccessor;
 
 public class BAEConnector extends AbstractCloudConnector<UriBasedServiceData> {
-	private EnvironmentAccessor environment = new EnvironmentAccessor();
 	
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-	public BAEConnector() {
-        super((Class) BAEConfigServiceInfoCreator.class);
-    }
+	private EnvironmentAccessor environment = new EnvironmentAccessor();
+	private ApplicationInstanceInfoCreator applicationInstanceInfoCreator = 
+			new ApplicationInstanceInfoCreator(environment);
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public BAEConnector() {
+		super((Class) BAEConfigServiceInfoCreator.class);
+	}
+
+	// SERVER_SORTWARE = bae/3.0
 	@Override
 	public boolean isInMatchingCloud() {
-		// SERVER_SORTWARE = bae/3.0
 		return environment.getEnvValue("SERVER_SOFTWARE") != null;
 	}
 
 	@Override
 	public ApplicationInstanceInfo getApplicationInstanceInfo() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return applicationInstanceInfoCreator.createApplicationInstanceInfo();
+		} catch (Exception e) {
+			throw new CloudException(e);
+		}
 	}
 
 	@Override
