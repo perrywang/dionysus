ddde2c1837d291f5b457df9519cd76c847a3223c
Lu, Jun <luj1985@gmail.com>
2015-03-08 11:47:18 +0800

    add actuator support

--- dionysus-webapp/src/main/java/org/dionysus/controller/ChatController.java
+++ dionysus-webapp/src/main/java/org/dionysus/controller/ChatController.java
@@ -5,14 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
 	@Autowired
-	private SimpMessageSendingOperations simp;
+	private SimpMessagingTemplate simp;
 
 	@MessageMapping("/chat/{id}")
 	public void handleChat(@Payload Message message, 
