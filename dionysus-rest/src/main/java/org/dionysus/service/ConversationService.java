2c32b0aede8aa1442d0f349165ff9a1e579a533d
Lu, Jun <luj1985@gmail.com>
2015-03-10 00:59:43 +0800

    remove conversation model

--- dionysus-rest/src/main/java/org/dionysus/service/ConversationService.java
+++
@@ -1,9 +0,0 @@
-package org.dionysus.service;
-
-import org.dionysus.repository.ConversationRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "conversations", path = "conversations")
-public interface ConversationService extends ConversationRepository {
-
-}
