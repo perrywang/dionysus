7626e48d97fe8388a16c03889afd46af16d1e275
Lu, Jun <luj1985@gmail.com>
2015-03-14 15:44:06 +0800

    remove lightadmin dependency, since it doesn't work on spring-boot 1.2.2 RELEASE

--- dionysus-admin/src/main/java/org/dionysus/admin/UserAdministration.java
+++
@@ -1,8 +0,0 @@
-package org.dionysus.admin;
-
-import org.dionysus.domain.User;
-import org.lightadmin.api.config.AdministrationConfiguration;
-
-public class UserAdministration extends AdministrationConfiguration<User> {
-
-}
