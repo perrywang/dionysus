7f895359eefd3e21f944259d68f9e84ffd5bc649
leancdf <leancdf@gmail.com>
2015-03-18 07:00:19 -0700

    Add security admin

--- dionysus-webapp/src/main/java/org/dionysus/WebSecurityConfiguration.java
+++ dionysus-webapp/src/main/java/org/dionysus/WebSecurityConfiguration.java
@@ -1,7 +1,9 @@
 package org.dionysus;
 
+import org.dionysus.auth.DionysusUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,7 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,13 +21,16 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	@Qualifier("dionysus")
-	private UserDetailsService securityUserService;
+	private DionysusUserDetailsService securityUserService;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		    .withUser("admin").password("admin").roles("ADMIN", "USER").and()
-			.withUser("user").password("password").roles("USER");
+//		auth.inMemoryAuthentication()
+//		    .withUser("admin").password("admin").roles("ADMIN", "USER").and()
+//			.withUser("user").password("password").roles("USER");
+		auth.userDetailsService(securityUserService).passwordEncoder(passwordEncoder());
+
+
 	}
 	
 	@Override
@@ -36,10 +42,23 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 //				.and()
 //			.httpBasic();
 
-		http.antMatcher("/api/v1/**")
-			.authorizeRequests()
-			.anyRequest()
-			.permitAll();
+//		http.antMatcher("/api/v1/**")
+//			.authorizeRequests()
+//			.anyRequest()
+//			.permitAll();
+		
+	    http.authorizeRequests()     
+            .antMatchers("/api/v1/admin/**")
+            .access("hasRole('ROLE_ADMIN')")
+            .and()
+            .formLogin()
+            //login.js is the login web page
+            .loginPage("/login").failureUrl("/login?error")
+			.usernameParameter("username")
+			.passwordParameter("password")
+			.and().logout().logoutSuccessUrl("/login?logout")
+			//403.js is the web page when access is denied
+			.and().exceptionHandling().accessDeniedPage("/403");
 	}
 	
 	@Override
@@ -50,5 +69,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.antMatchers("/styles/**")
 			.antMatchers("/images/**")
 			.antMatchers("/scripts/**");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
 	}
 }
