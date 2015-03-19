da6966a688b4670a41fdaf37b47c884ad66d7596
Lu, Jun <luj1985@gmail.com>
2015-03-20 00:55:47 +0800

    split home page

---
+++ dionysus-client/yo/app/scripts/modules/home.js
@@ -0,0 +1,36 @@
+Dionysus.module('Home.Show', function(Home, Dionysus, Backbone, Marionette, $, _) {
+  var Page = Backbone.Model.extend({
+    urlRoot: "api/v1/pages"
+  });
+
+  var PageCollection = Backbone.Collection.extend({
+    url: "api/v1/pages",
+    parse: function(response) {
+      return response._embedded.pages;
+    },
+    model: Page
+  });
+
+  var SliderView = Marionette.ItemView.extend({
+    template: '#slide-template',
+    tagName: 'li'
+  });
+
+  var SlidersView = Marionette.CompositeView.extend({
+    template: '#slides-template',
+    childView: SliderView,
+    childViewContainer: 'ul.slides',
+    className: 'slider'
+  });
+
+  Dionysus.addInitializer(function(options) {
+    var pages = new PageCollection();
+    var sliders = new SlidersView({ collection: pages });
+
+    pages.fetch().then(function() {
+      $('.slider').slider({full_width: true});
+    });
+
+    Dionysus.mainRegion.show(sliders);
+  });
+});
