cbfad655a25e9988c7b8494d2d911694767abbc7
Perry Wang <perrywangsh@gmail.com>
2015-04-01 15:55:54 +0800

    add PsychTestCalculateController to calculate test result

---
+++ dionysus-webapp/src/main/java/com/huixinpn/dionysus/controller/PsychTestCalculateController.java
@@ -0,0 +1,17 @@
+package com.huixinpn.dionysus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class PsychTestCalculateController {
+    @RequestMapping(value = "/calculate", method = RequestMethod.POST)
+    public void calculate(List<Long> options){
+        //TODO we only need get a list including user selected PsychTestItemOption ids
+    }
+
+
+}
