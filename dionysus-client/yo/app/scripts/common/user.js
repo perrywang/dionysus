d77c5d212a753f68f3e3355efff5c47868e57c5f
Ryan Huang <hhao3000@gmail.com>
2015-04-08 11:32:41 +0800

    add site page

---
+++ dionysus-client/yo/app/scripts/common/user.js
@@ -0,0 +1,43 @@
+Dionysus.module('Entities', function(Entities, Dionysus, Backbone, Marionette, $) {
+  'use strict';
+
+  Entities.User = Backbone.Model.extend({
+    urlRoot: '/api/v1/users'
+  });
+
+  Entities.UserCollection = Backbone.PageableCollection.extend({
+    model: Entities.User,
+    url: '/api/v1/users',
+    parse: function(response) {
+      var embedded = response._embedded;
+      return embedded ? embedded.users : [];
+    },
+    state: {
+      firstPage: 0
+    },
+    queryParams: {
+      currentPage: 'page',
+      pageSize: 'size'
+    }
+  });
+
+  Dionysus.reqres.setHandler('user:entities', function() {
+    var users = new Entities.UserCollection();
+    var defer = $.Deferred();
+    users.fetch().then(function() {
+      defer.resolve(users);
+    });
+    return defer.promise();
+  });
+
+  Dionysus.reqres.setHandler('user:entity', function(id) {
+    var user = new Entities.User({id: id});
+    var defer = $.Deferred();
+    user.fetch().then(function() {
+      defer.resolve(user);
+    });
+    return defer.promise();
+  });
+
+});
+
