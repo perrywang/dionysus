154d643a2f26d3cb2c8a0ac233c326efc6a3853a
perrywang <perrywangsh@gmail.com>
2015-04-14 01:42:10 +0800

    revert da-feng checkin resulting in messy code

--- dionysus-core/src/main/resources/data.sql
+++ dionysus-core/src/main/resources/data.sql
@@ -1,64 +1,64 @@
-insert into categories (id, name) values(1, 'äº²å­�å…³ç³»');
-insert into categories (id, name) values(2, 'å®¶åº­å…³ç³»');
-insert into categories (id, name) values(3, 'å©šå§»å…³ç³»');
-insert into categories (id, name) values(4, 'è�Œä¸šå¿ƒç�†');
-insert into categories (id, name) values(5, 'ä¸ªäººæˆ�é•¿');
+insert into categories (id, name) values(1, '亲子关系');
+insert into categories (id, name) values(2, '家庭关系');
+insert into categories (id, name) values(3, '婚姻关系');
+insert into categories (id, name) values(4, '职业心理');
+insert into categories (id, name) values(5, '个人成长');
 
-insert into articles (id, category_id, title, summary, body) values (1, 4, 'æŒ«æŠ˜å¯¹æˆ‘ä»¬æ„�å‘³ç�€ä»€ä¹ˆï¼Ÿ', 'å¿ƒç�†å­¦ä¸­æœ‰ä¸€ç§�è¯´æ³•ï¼Œè¦�æƒ³èŽ·å¾—æƒ…ç»ªä¸Šçš„æˆ�ç†Ÿï¼Œéœ€è¦�ç»�åŽ†â€œæ�°åˆ°å¥½å¤„çš„æŒ«æŠ˜â€�ã€‚æˆ‘ä»¬æ—©æœŸéƒ½ä¼šæœ‰å¾ˆå¤šç�†æƒ³å’Œæ„¿æœ›ï¼Œéœ€è¦�é€šè¿‡å�ˆé€‚ä½†ä¸�è¿‡åˆ†çš„æŒ«æŠ˜ï¼Œæ‰�èƒ½åˆ†è¾¨æ„¿æœ›å’ŒçŽ°å®žçš„åŒºåˆ«ï¼ŒèŽ·å¾—çŽ°å®žæ„Ÿå’Œè¡ŒåŠ¨åŠ›ã€‚é€‚å½“çš„æŒ«æŠ˜è®©æˆ‘ä»¬æ˜Žç™½ï¼Œæ»¡è¶³æ„¿æœ›ä¹‹å‰�è¦�å…ˆæœ‰è¡ŒåŠ¨ã€‚', 'æ­£æ–‡å†…å®¹');
-insert into articles (id, category_id, title, summary, body) values (2, 4, 'ä¸Šç�­æ��æƒ§ç—‡æ€Žä¹ˆå…‹æœ�', 'æ‰€è°“é‡‘æ— è¶³èµ¤ï¼Œäººæ— å®Œäººã€‚æˆ‘ä»¬ä¸�å�¯èƒ½è¿Žå�ˆæ¯�ä¸ªäººçš„æ€§æ ¼ï¼Œä½ ä¸�å–œæ¬¢ä»–äººçš„æ€§æ ¼ï¼Œä½ è®¤ä¸ºæ¯�ä¸€ä¸ªäººéƒ½å–œæ¬¢ä½ å�—ï¼Ÿè¦�å­¦ä¼šå®¹çº³ä»–äººï¼Œæœ‰å®¹ä¹ƒå¤§ã€‚ä¹Ÿä¸�è¦�æ€»æ˜¯åŽ»è¯„è®ºä»–äººï¼Œå�šå¥½è‡ªå·±å°±è¡Œäº†ï¼Œä»–å�šå¾—å°‘æ‹¿å¾—å¤šï¼Œä»–ä¼šæˆ�ä¸–ç•Œé¦–å¯Œå�—ï¼Ÿä¸Žå…¶è®¨è®ºè¿™äº›æ²¡ç”¨çš„ï¼Œè¿˜ä¸�å¦‚åŠªåŠ›å·¥ä½œï¼Œç›¸ä¿¡è‡ªå·±æœªæ�¥å�šäº†ä»–ä¸Šå�¸å°±å�¯ä»¥æ‰£ä»–å·¥èµ„äº†ï¼Œå‘µå‘µï¼Œå¼€çŽ©ç¬‘çš„ã€‚ä¸�è¿‡è¿˜æ˜¯è¦�é� è‡ªå·±åŠªåŠ›å·¥ä½œã€‚', 'æ­£æ–‡å†…å®¹');
-insert into articles (id, category_id, title, summary, body) values (3, 5, 'å¦‚ä½•æ”¾ä¸‹', 'å¦‚æžœå¤„åœ¨ä¸€èˆ¬çš„å±‚é�¢ï¼Œçœ‹åˆ°ä½ çš„æ��è¿°ï¼Œè€Œå�Žå�Œæƒ…ä½ çš„é�­é�‡ï¼Œæˆ‘æƒ³ä½ ä¼šè§‰å¾—è¢«å®‰æ…°ï¼Œä¼šå¾—åˆ°ä¸€æ—¶çš„å¹³è¡¡ã€‚ä½†è€Œå�Žï¼Œå½“ä½ å›žåˆ°äººé™…ç¾¤ä½“ä¸­ï¼Œä½ ä»�ä¼šè¢«è¿™æ ·çš„å¢ƒé�‡åŒ…å›´ã€‚æ‰€ä»¥ï¼Œæˆ‘æƒ³â€œåœ¨ä½ çš„ä¼¤å�£ä¸Šæ’’ç›�â€�ï¼�ä¿—è¯�è¯´â€œå�¯æ€œä¹‹äººå¿…æœ‰å�¯æ�¨ä¹‹å¤„ï¼Œå�¯æ�¨ä¹‹äººä¹Ÿå¿…ç„¶æœ‰å�¯æ€œä¹‹å¤„â€�ï¼Œä½œä¸ºä¸€ä¸ªå�¯æ€œäººï¼Œä½ è‚¯å®šæœ‰äº›å�¯æ�¨ä¹‹å¤„ï¼Œè€Œäººçš„æœ¬æ€§ä¹‹ä¸€æ˜¯â€œäººéƒ½ä¸�æ„¿æ„�æ‰¿è®¤è‡ªå·±é”™äº†â€�ï¼Œæ‰€ä»¥ï¼Œå¦‚æžœæƒ³æ”¹å�˜çŽ°çŠ¶ï¼Œè®©è‡ªå·±ä½“éªŒåˆ°å¹³å’Œçš„å¿«ä¹�ï¼Œé‚£å°±å‹‡æ•¢çš„æ‰¿æ‹…æ”¹å�˜çš„ç—›è‹¦ï¼Œæ‰¾å¯»ä¸‹è‡ªå·±çš„åŠ£åŠ¿ï¼Œè€Œå�Žæ”¹å�˜ã€‚', 'æ­£æ–‡å†…å®¹');
-insert into articles (id, category_id, title, summary, body) values (4, 5, 'å¦‚ä½•å®Œç¾Žåœ°å¤±è´¥', 'çŸ¥æ€§ä¸Šï¼Œæˆ‘ä»¬çŸ¥é�“å¤±è´¥èƒ½ä½¿æˆ‘ä»¬é¢†æ‚Ÿæœºé�‡ï¼Œä»Žè€Œä¸ºæˆ‘ä»¬äººç”Ÿæœ€ç»ˆçš„èƒœåˆ©å¯¼èˆªã€‚å› æ­¤ï¼Œå½“æ‹’ç»�é™�ä¸´ã€�è€�æ�¿è¯´ä¸�ï¼›å½“ä½ æŠ•èµ„å¤±åˆ©ã€�ä¸¢äº†æˆ¿å­�ï¼Œæˆ–è€…å¤±åŽ»äº†ä»»ä½•ä½ æƒ³è¦�çš„ä¸œè¥¿æ—¶ï¼Œæ”¾æ…¢é€Ÿåº¦ï¼Œçœ‹çœ‹æœ‰å“ªé‡Œä¸�å¯¹ã€‚', 'æ­£æ–‡å†…å®¹');
-insert into articles (id, category_id, title, summary, body) values (5, 5, 'è�¨æ��äºšè‡ªæˆ‘æˆ�é•¿å·¥ä½œå�Š', 'çœŸæ­£çš„åŠ›é‡�ç»�ä¸�æ˜¯è¦�å¼ºã€�è®¡è¾ƒã€�é˜²å¾¡ï¼Œé‚£æ ·å�ªä¼šè®©è‡ªå·±ç´¯ä¸Žå­¤ç‹¬ã€‚çœŸæ­£çš„åŠ›é‡�æ˜¯æ¸©æš–ã€‚ä½ å�¯ä»¥å�šä¸€ä¸ªå†…å¿ƒæœ‰åŠ›é‡�çš„äººï¼Œå�¯ä»¥æ¸©æš–è‡ªå·±ï¼Œä¹Ÿæ¸©æš–èº«è¾¹çš„äººã€‚å�¯ä»¥æ”¾ä¸‹æŠ—æ‹’ä¸ŽæŽ§åˆ¶ï¼Œæ´»å‡ºçˆ±ä¸Žå…‰ã€‚è¿™ä¸ªä¸–ç•Œä¸Šçš„çƒ¦æ�¼ï¼Œå½’æ ¹ç»“åº•å°±æ˜¯å¯¹è‡ªå·±çš„ä¸�äº†è§£ã€‚æ‰�ä¼šå»¶ä¼¸å‡ºä¸€ç³»åˆ—çš„é—®é¢˜ï¼šå¯¹è‡ªå·±è¦�æ±‚é«˜ã€�æ•�æ„Ÿã€�æ‹–å»¶ã€�å®Œç¾Žä¸»ä¹‰ã€�æŠ‘éƒ�ã€�å¿§ä¼¤ã€�æŒ«è´¥ï¼Œç»§è€Œè‡ªæˆ‘å�¦å®šã€‚ä»¥ä¸ºå¾—åˆ°é€šè¿‡åŠªåŠ›å°±ä¼šå¾—åˆ°æ›´å¤šçš„æˆ�å°±ï¼Œå°±ä¼šå¾—åˆ°è®¤å�¯ã€‚', 'æ­£æ–‡å†…å®¹');
+insert into articles (id, category_id, title, summary, body) values (1, 4, '挫折对我们意味着什么？', '心理学中有一种说法，要想获得情绪上的成熟，需要经历“恰到好处的挫折”。我们早期都会有很多理想和愿望，需要通过合适但不过分的挫折，才能分辨愿望和现实的区别，获得现实感和行动力。适当的挫折让我们明白，满足愿望之前要先有行动。', '正文内容');
+insert into articles (id, category_id, title, summary, body) values (2, 4, '上班恐惧症怎么克服', '所谓金无足赤，人无完人。我们不可能迎合每个人的性格，你不喜欢他人的性格，你认为每一个人都喜欢你吗？要学会容纳他人，有容乃大。也不要总是去评论他人，做好自己就行了，他做得少拿得多，他会成世界首富吗？与其讨论这些没用的，还不如努力工作，相信自己未来做了他上司就可以扣他工资了，呵呵，开玩笑的。不过还是要靠自己努力工作。', '正文内容');
+insert into articles (id, category_id, title, summary, body) values (3, 5, '如何放下', '如果处在一般的层面，看到你的描述，而后同情你的遭遇，我想你会觉得被安慰，会得到一时的平衡。但而后，当你回到人际群体中，你仍会被这样的境遇包围。所以，我想“在你的伤口上撒盐”！俗话说“可怜之人必有可恨之处，可恨之人也必然有可怜之处”，作为一个可怜人，你肯定有些可恨之处，而人的本性之一是“人都不愿意承认自己错了”，所以，如果想改变现状，让自己体验到平和的快乐，那就勇敢的承担改变的痛苦，找寻下自己的劣势，而后改变。', '正文内容');
+insert into articles (id, category_id, title, summary, body) values (4, 5, '如何完美地失败', '知性上，我们知道失败能使我们领悟机遇，从而为我们人生最终的胜利导航。因此，当拒绝降临、老板说不；当你投资失利、丢了房子，或者失去了任何你想要的东西时，放慢速度，看看有哪里不对。', '正文内容');
+insert into articles (id, category_id, title, summary, body) values (5, 5, '萨提亚自我成长工作坊', '真正的力量绝不是要强、计较、防御，那样只会让自己累与孤独。真正的力量是温暖。你可以做一个内心有力量的人，可以温暖自己，也温暖身边的人。可以放下抗拒与控制，活出爱与光。这个世界上的烦恼，归根结底就是对自己的不了解。才会延伸出一系列的问题：对自己要求高、敏感、拖延、完美主义、抑郁、忧伤、挫败，继而自我否定。以为得到通过努力就会得到更多的成就，就会得到认可。', '正文内容');
 
-insert into comments (id, article_id, content) values (1, 1, 'æµ‹è¯•ç”¨Comment');
-insert into comments (id, article_id, content) values (2, 1, 'ç¬¬äºŒæ�¡æµ‹è¯•ç”¨Comment');
-insert into comments (id, article_id, content) values (3, 2, 'æµ‹è¯•ç”¨Comment');
+insert into comments (id, article_id, content) values (1, 1, '测试用Comment');
+insert into comments (id, article_id, content) values (2, 1, '第二条测试用Comment');
+insert into comments (id, article_id, content) values (3, 2, '测试用Comment');
 
-insert into modules (id, title, subtitle, href, image, body) values (1, 'æ±‡å¿ƒç½‘', 'æ±‡å¿ƒç½‘ç»œä¸­å¿ƒ', '/app/site', '/public/images/slide-2.jpeg', 'å…³æ€€æ— å¤„ä¸�åœ¨');
-insert into modules (id, title, subtitle, href, image, body) values (2, 'å¿ƒç�†æœ�åŠ¡å°�ç«™', 'å¿ƒç�µé�¿é£Žæ¸¯.æ±‡å¿ƒå¿ƒç�†', '/app/colleges', '/public/images/slide-3.jpeg', 'å›½å†…æœ€ä¸“ä¸šçš„æ ¡å›­å¿ƒç�†æœ�åŠ¡æ��ä¾›å•†');
-insert into modules (id, title, subtitle, href, image, body) values (3, 'æ±‡å¿ƒå¿ƒç�†å­¦é™¢', 'ä¸“ä¸šå¿ƒç�†ä»Žä¸šäººå‘˜å­µåŒ–åŸºåœ°', '/app/about', '/public/images/slide-1.jpeg', 'æ±‡å¿ƒå¿ƒç�†å­¦é™¢ï¼Œä¸“ä¸šæº�äºŽæ�ƒå¨�');
+insert into modules (id, title, subtitle, href, image, body) values (1, '汇心网', '汇心网络中心', '/app/site', '/public/images/slide-2.jpeg', '关怀无处不在');
+insert into modules (id, title, subtitle, href, image, body) values (2, '心理服务小站', '心灵避风港.汇心心理', '/app/colleges', '/public/images/slide-3.jpeg', '国内最专业的校园心理服务提供商');
+insert into modules (id, title, subtitle, href, image, body) values (3, '汇心心理学院', '专业心理从业人员孵化基地', '/app/about', '/public/images/slide-1.jpeg', '汇心心理学院，专业源于权威');
 
-insert into pages (id, name, title, body) values (1, 'about', 'About Us', 'å…³äºŽæˆ‘ä»¬çš„è¯¦ç»†é¡µé�¢');
+insert into pages (id, name, title, body) values (1, 'about', 'About Us', '关于我们的详细页面');
 
-insert into menu (id, name, href) values (1, 'å¿ƒç�†ä½“éªŒ', '/app/experience');
-insert into menu (id, name, href) values (2, 'åœ¨çº¿å¿ƒç�†å’¨è¯¢', '/app/consult');
-insert into menu (id, name, href) values (3, 'å¿ƒç�†çŸ¥è¯†', '/app/articles');
-insert into menu (id, name, href) values (4, 'å¿ƒç�†è¯¾ç¨‹', '/app/cources');
+insert into menu (id, name, href) values (1, '心理体验', '/app/experience');
+insert into menu (id, name, href) values (2, '在线心理咨询', '/app/consult');
+insert into menu (id, name, href) values (3, '心理知识', '/app/articles');
+insert into menu (id, name, href) values (4, '心理课程', '/app/cources');
 
-insert into psychtests(id, title, description) values (1, 'ä½ æ˜¯ä¸€ä¸ªå¿ƒåœ°å–„è‰¯çš„äººå�—ï¼Ÿ', 'æµ‹è¯•ä½ æ˜¯ä¸€ä¸ªå¿ƒåœ°å–„è‰¯çš„äººè¿˜æ˜¯ä¸€ä¸ªè€�å¥½äºº');
+insert into psychtests(id, title, description) values (1, '你是一个心地善良的人吗？', '测试你是一个心地善良的人还是一个老好人');
 
-insert into psychtestitems(id, test_id, description) values (1, 1, 'çœ‹åˆ°æœ‰äººåœ¨ä½ é�¢å‰�æŠ¢åŠ«ä½ ä¼šæ€Žä¹ˆå�šï¼Ÿ');
-insert into psychtestitemoptions(id, item_id, description) values (1, 1, 'è£…ä½œæ²¡çœ‹è§�');
-insert into psychtestitemoptions(id, item_id, description) values (2, 1, 'ä¸Šå‰�æ��æ–—');
-insert into psychtestitemoptions(id, item_id, description) values (3, 1, 'çŠ¹è±«äº†ä¸€ä¸‹ï¼Œç„¶å�Žä½Žå¤´ä¸�è¯­');
+insert into psychtestitems(id, test_id, description) values (1, 1, '看到有人在你面前抢劫你会怎么做？');
+insert into psychtestitemoptions(id, item_id, description) values (1, 1, '装作没看见');
+insert into psychtestitemoptions(id, item_id, description) values (2, 1, '上前搏斗');
+insert into psychtestitemoptions(id, item_id, description) values (3, 1, '犹豫了一下，然后低头不语');
 
-insert into psychtestitems(id, test_id, description) values (2, 1, 'çœ‹åˆ°æœ‰äººåœ¨ä½ é�¢å‰�ä¹žè®¨ä½ ä¼šæ€Žä¹ˆå�šï¼Ÿ');
-insert into psychtestitemoptions(id, item_id, description) values (4, 2, 'ç«‹åˆ»ç»™é’±');
-insert into psychtestitemoptions(id, item_id, description) values (5, 2, 'åˆ¤æ–­ä¸‹æ˜¯ä¸�æ˜¯éª—å­�ï¼Œç„¶å�Žå†�å†³å®šç»™ä¸�ç»™');
-insert into psychtestitemoptions(id, item_id, description) values (6, 2, 'æ‹…å¿ƒè¢«éª—ï¼ŒçŠ¹è±«ä¸‹èµ°å¼€');
+insert into psychtestitems(id, test_id, description) values (2, 1, '看到有人在你面前乞讨你会怎么做？');
+insert into psychtestitemoptions(id, item_id, description) values (4, 2, '立刻给钱');
+insert into psychtestitemoptions(id, item_id, description) values (5, 2, '判断下是不是骗子，然后再决定给不给');
+insert into psychtestitemoptions(id, item_id, description) values (6, 2, '担心被骗，犹豫下走开');
 
-insert into psychtestitems(id, test_id, description) values (3, 1, 'ä½ ä¼šå�‘æ…ˆå–„æœºæž„æ��æ¬¾å�—ï¼Ÿ');
-insert into psychtestitemoptions(id, item_id, description) values (7, 3, 'ä»Žä¸�');
-insert into psychtestitemoptions(id, item_id, description) values (8, 3, 'å¾ˆå°‘');
-insert into psychtestitemoptions(id, item_id, description) values (9, 3, 'ç»�å¸¸');
+insert into psychtestitems(id, test_id, description) values (3, 1, '你会向慈善机构捐款吗？');
+insert into psychtestitemoptions(id, item_id, description) values (7, 3, '从不');
+insert into psychtestitemoptions(id, item_id, description) values (8, 3, '很少');
+insert into psychtestitemoptions(id, item_id, description) values (9, 3, '经常');
 
-insert into psychtestitems(id, test_id, description) values (4, 1, 'äº²çœ¼ç›®ç�¹äº†ä¸€åœºè½¦ç¥¸ï¼Œä½ å¤šä¹…èƒ½å¤Ÿå¿˜è®°ï¼Ÿ');
-insert into psychtestitemoptions(id, item_id, description) values (10, 4, 'ä¸€å¤©');
-insert into psychtestitemoptions(id, item_id, description) values (11, 4, 'ä¸€å‘¨');
-insert into psychtestitemoptions(id, item_id, description) values (12, 4, 'ä¸€ä¸ªæœˆ');
+insert into psychtestitems(id, test_id, description) values (4, 1, '亲眼目睹了一场车祸，你多久能够忘记？');
+insert into psychtestitemoptions(id, item_id, description) values (10, 4, '一天');
+insert into psychtestitemoptions(id, item_id, description) values (11, 4, '一周');
+insert into psychtestitemoptions(id, item_id, description) values (12, 4, '一个月');
 
 -- password: 'password'
-insert into users (id, username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, about, avatar)
-values (1, 'admin','$2a$10$56XBkZOecmVo27bDByl4t.Y49T6ps.mJZf1ptSdAWf1fAICftXzdG', TRUE, TRUE, TRUE, TRUE, 'é«˜çº§æ³¨å†Œå¿ƒç�†å’¨è¯¢å¸ˆï¼Œæ“…é•¿æŠ‘éƒ�ç—‡å¼•å¯¼å’Œå’¨è¯¢', '/public/images/placeholder.png');
+insert into users (id, username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled)
+values (1, 'admin','$2a$10$56XBkZOecmVo27bDByl4t.Y49T6ps.mJZf1ptSdAWf1fAICftXzdG', TRUE, TRUE, TRUE, TRUE);
+
+insert into users (id, username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled)
+values (2, 'test1','$2a$10$56XBkZOecmVo27bDByl4t.Y49T6ps.mJZf1ptSdAWf1fAICftXzdG', TRUE, TRUE, TRUE, TRUE);
 
 insert into users (id, username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, about, avatar)
-values (2, 'test1','$2a$10$56XBkZOecmVo27bDByl4t.Y49T6ps.mJZf1ptSdAWf1fAICftXzdG', TRUE, TRUE, TRUE, TRUE, 'é«˜çº§æ³¨å†Œå¿ƒç�†å’¨è¯¢å¸ˆï¼Œæ“…é•¿æŠ‘éƒ�ç—‡å¼•å¯¼å’Œå’¨è¯¢', '/public/images/placeholder.png');
+values (3, 'test2','$2a$10$56XBkZOecmVo27bDByl4t.Y49T6ps.mJZf1ptSdAWf1fAICftXzdG', TRUE, TRUE, TRUE, TRUE, '高级注册心理咨询师，擅长抑郁症引导和咨询', '/public/images/placeholder.png');
 
 insert into users (id, username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, about, avatar)
-values (3, 'test2','$2a$10$56XBkZOecmVo27bDByl4t.Y49T6ps.mJZf1ptSdAWf1fAICftXzdG', TRUE, TRUE, TRUE, TRUE, 'é«˜çº§æ³¨å†Œå¿ƒç�†å’¨è¯¢å¸ˆï¼Œæ“…é•¿æŠ‘éƒ�ç—‡å¼•å¯¼å’Œå’¨è¯¢', '/public/images/placeholder.png');
-
-insert into users (id, username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, about, avatar)
-values (4, 'wp','$2a$10$56XBkZOecmVo27bDByl4t.Y49T6ps.mJZf1ptSdAWf1fAICftXzdG', TRUE, TRUE, TRUE, TRUE, 'é«˜çº§æ³¨å†Œå¿ƒç�†å’¨è¯¢å¸ˆï¼Œæ“…é•¿å®¶åº­å…³ç³»å’¨è¯¢å’Œè°ƒèŠ‚', '/public/images/placeholder.png');
+values (4, 'wp','$2a$10$56XBkZOecmVo27bDByl4t.Y49T6ps.mJZf1ptSdAWf1fAICftXzdG', TRUE, TRUE, TRUE, TRUE, '高级注册心理咨询师，擅长家庭关系咨询和调节', '/public/images/placeholder.png');
 
 insert into consultants (id) values (4);
 insert into consultants (id) values (3);
