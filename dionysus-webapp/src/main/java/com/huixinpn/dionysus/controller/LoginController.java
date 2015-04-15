201a236c6d898efdb1727705637bb69e0320b5b0
perrywang <perrywangsh@gmail.com>
2015-04-16 01:35:05 +0800

    use spring HttpSessionSecurityContext to store securitycontext after login successfully

--- dionysus-webapp/src/main/java/com/huixinpn/dionysus/controller/LoginController.java
+++ dionysus-webapp/src/main/java/com/huixinpn/dionysus/controller/LoginController.java
@@ -6,8 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.web.context.HttpRequestResponseHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +27,8 @@ public class LoginController {
   private static Logger logger = LoggerFactory.getLogger(LoginController.class);
   private UserService userService;
 
+  private HttpSessionSecurityContextRepository sessionSecurityContextRepository = new HttpSessionSecurityContextRepository();
+
   @Autowired
   public LoginController(UserService service) {
     this.userService = service;
@@ -31,10 +40,14 @@ public class LoginController {
   User login(@RequestBody User user, HttpServletRequest request, HttpServletResponse response) {
 
     User loginedUser = userService.sign(user.getUsername(), user.getPassword());
-    request.getSession();
-    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(loginedUser, user.getPassword(), user.getAuthorities());
-    SecurityContextHolder.getContext().setAuthentication(auth);
+    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), loginedUser.getAuthorities());
+    SecurityContext context = SecurityContextHolder.getContext();
+    context.setAuthentication(auth);
+    HttpRequestResponseHolder requestResponseHolder = new HttpRequestResponseHolder(request, response);
+    sessionSecurityContextRepository.loadContext(requestResponseHolder);
+    sessionSecurityContextRepository.saveContext(context, requestResponseHolder.getRequest(), requestResponseHolder.getResponse());
     return loginedUser;
+
   }
 
   @RequestMapping(value = "/logout", method = RequestMethod.GET)
