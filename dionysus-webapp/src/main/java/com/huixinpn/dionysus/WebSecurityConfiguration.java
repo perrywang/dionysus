b1f930eec320b00f590285c380eb2b75df9831bc
Perry Wang <perrywangsh@gmail.com>
2015-04-16 20:07:22 +0800

    remove unnecessary code

--- dionysus-webapp/src/main/java/com/huixinpn/dionysus/WebSecurityConfiguration.java
+++ dionysus-webapp/src/main/java/com/huixinpn/dionysus/WebSecurityConfiguration.java
@@ -1,37 +1,23 @@
 package com.huixinpn.dionysus;
 
-import com.huixinpn.dionysus.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  private UserService securityUserService;
-
-  @Autowired
-  private PasswordEncoder encoder;
-
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(securityUserService).passwordEncoder(encoder);
-  }
-
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable()
         .authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/api/v1/login").permitAll()
+        .antMatchers(HttpMethod.GET, "/", "/themes/**", "/public/**", "/styles/**", "/fonts/**", "/images/**", "/scripts/**").permitAll()
         .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
         .regexMatchers(HttpMethod.GET, ".*/api/v1/(users|consultants|inbox|appointments|notifications|profiles)(/|/\\d+)?.*").authenticated()
         .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
@@ -39,13 +25,4 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         .antMatchers(HttpMethod.DELETE, "/api/v1/**").authenticated();
   }
 
-
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    web.ignoring()
-        .antMatchers(HttpMethod.POST, "/api/v1/login")
-        .antMatchers(HttpMethod.GET, "/",
-            "/themes/**", "/public/**",
-            "/styles/**", "/fonts/**", "/images/**", "/scripts/**");
-  }
 }
