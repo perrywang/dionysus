ac75e69be308078f9bec45b5b2a16ce596aca727
Lu, Jun <luj1985@gmail.com>
2015-04-19 15:32:23 +0800

    add default constructor for Page

--- dionysus-core/src/main/java/com/huixinpn/dionysus/domain/site/Page.java
+++ dionysus-core/src/main/java/com/huixinpn/dionysus/domain/site/Page.java
@@ -7,6 +7,7 @@ import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +16,7 @@ import com.huixinpn.dionysus.domain.user.User;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "pages")
