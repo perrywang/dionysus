fc903f75b630d664970b59b919a98aa54c4a5de8
Lu, Jun <luj1985@gmail.com>
2015-04-19 20:57:08 +0800

    extract out validation rules

--- dionysus-client/yo/app/scripts/admin/article-management.js
+++ dionysus-client/yo/app/scripts/admin/article-management.js
@@ -1,6 +1,13 @@
 Dionysus.module('AdminArticle', function(Article, Dionysus, Backbone, Marionette, $) {
   'use strict';
 
+  var validationRules = {
+    title:      { identifier: 'title',      rules: [{ type: 'empty', prompt: 'Please enter a title' }] },
+    category:   { identifier: 'category',   rules: [{ type: 'empty', prompt: 'Please enter a category' }] },
+    summary:    { identifier: 'summary',    rules: [{ type: 'empty', prompt: 'Please enter a summary' }] },
+    body:       { identifier: 'body',       rules: [{ type: 'empty', prompt: 'Please enter article body' }] }
+  };
+
   var ArticleView = Marionette.ItemView.extend({ 
     template: '#admin-article-tpl',
     tagName: 'li',
@@ -23,41 +30,16 @@ Dionysus.module('AdminArticle', function(Article, Dionysus, Backbone, Marionette
     },
     serializeData: function(){
       var data = this.model.toJSON();
-      data.categories = this.categories;
+      var category = data.category;
+      if (_.isObject(category)) {
+        data.category = category.id;
+      }
+      data.categories = [{'xxx': 1}, {'yyy' : 2}];
       return data;
     },
     onRender: function() {
       this.$('select.dropdown').dropdown();
-      this.$el.form({
-        title: {
-          identifier: 'title',
-          rules: [{
-            type: 'empty',
-            prompt: 'Please enter a title'
-          }]
-        },
-        categoryId: {
-          identifier: 'categoryId',
-          rules: [{
-            type: 'empty',
-            prompt: 'Please enter a category'
-          }]
-        },
-        summary: {
-          identifier: 'summary',
-          rules: [{
-            type: 'empty',
-            prompt: 'Please enter a summary'
-          }]
-        },
-        body: {
-          identifier: 'body',
-          rules: [{
-            type: 'empty',
-            prompt: 'Please enter article body'
-          }]
-        }
-      });
+      this.$el.form();
 
       this.$el.form('set values', this.model.toJSON());
       this.$('.editor').editable({
@@ -99,9 +81,7 @@ Dionysus.module('AdminArticle', function(Article, Dionysus, Backbone, Marionette
     },
     editArticle: function(id) {
       Dionysus.request('article:instance', id).then(function(article) {
-        var category = article.link('category');
-        console.log(category);
-        category.fetch();
+        // category.fetch();
         var editor = new ArticleEditorView({ model: article, categories: []});
         Dionysus.mainRegion.show(editor);
       });
