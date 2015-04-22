5563177299944aafc9c97eff0af10d01af67fe5a
Perry Wang <perrywangsh@gmail.com>
2015-04-22 16:40:52 +0800

    add findBy methods for CourseRepository

--- dionysus-core/src/main/java/com/huixinpn/dionysus/repository/CourseRepository.java
+++ dionysus-core/src/main/java/com/huixinpn/dionysus/repository/CourseRepository.java
@@ -1,11 +1,22 @@
 package com.huixinpn.dionysus.repository;
 
+import com.huixinpn.dionysus.domain.course.CourseCategory;
+import com.huixinpn.dionysus.domain.user.Consultant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.huixinpn.dionysus.domain.course.Course;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+  Page<Course> findByCategory(@Param("cid") CourseCategory category, Pageable pagable);
+
+  Page<Course> findByConsultant(@Param("id") Consultant consultant, Pageable pageable);
 
 }
