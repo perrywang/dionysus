5dc2c142fa379ed64ff705c092ee887a0af43732
perrywang <perrywangsh@gmail.com>
2015-04-25 22:48:51 +0800

    Pageable method should contain Pagable argument

--- dionysus-core/src/main/java/com/huixinpn/dionysus/repository/NotificationRepository.java
+++ dionysus-core/src/main/java/com/huixinpn/dionysus/repository/NotificationRepository.java
@@ -3,10 +3,11 @@ package com.huixinpn.dionysus.repository;
 import com.huixinpn.dionysus.domain.user.Notification;
 import com.huixinpn.dionysus.domain.user.User;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-  Page<Notification> findByInboxUser(User user);
+  Page<Notification> findByInboxUser(User user,Pageable pageable);
 }
