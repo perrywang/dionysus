d0eac5c3a7300e7fef2bdc43c3016643f8107d42
Ryan Huang <hhao3000@gmail.com>
2015-04-28 22:33:10 +0800

    format code

--- dionysus-core/src/main/java/com/huixinpn/dionysus/domain/user/User.java
+++ dionysus-core/src/main/java/com/huixinpn/dionysus/domain/user/User.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.huixinpn.dionysus.auth.PasswordListener;
 import com.huixinpn.dionysus.domain.AbstractDionysusPersistable;
 import com.huixinpn.dionysus.domain.course.Course;
-
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
@@ -25,81 +23,81 @@ import java.util.Set;
 @EntityListeners(PasswordListener.class)
 public class User extends AbstractDionysusPersistable implements UserDetails {
 
-  private static final long serialVersionUID = 6574790333326442416L;
+    private static final long serialVersionUID = 6574790333326442416L;
 
-  @NotNull
-  @Size(min = 4, max = 40)
-  @Column(name = "username", unique = true)
-  private String username;
+    @NotNull
+    @Size(min = 4, max = 40)
+    @Column(name = "username", unique = true)
+    private String username;
 
-  @Transient
-  transient private String password;
+    @Transient
+    transient private String password;
 
-  @JsonIgnore
-  @Column(name = "password")
-  private String encryptedPassword;
+    @JsonIgnore
+    @Column(name = "password")
+    private String encryptedPassword;
 
-  @Column(name = "email")
-  private String email;
+    @Column(name = "email")
+    private String email;
 
-  @Column(name = "gender")
-  private String gender;
-  
-  @Column(name = "age")
-  private String age;
-  
-  @Lob
-  @Column(name = "address")
-  private String address;
-  
-  @Column(name = "mobile")
-  private String mobile;
-  
-  @Column(name = "landline")
-  private String landline;
-  
-  @NotNull
-  @Column(name = "account_non_expired")
-  private boolean accountNonExpired;
+    @Column(name = "gender")
+    private String gender;
 
-  @NotNull
-  @Column(name = "account_non_locked")
-  private boolean accountNonLocked;
+    @Column(name = "age")
+    private String age;
 
-  @NotNull
-  @Column(name = "credentials_non_expired")
-  private boolean credentialsNonExpired;
+    @Lob
+    @Column(name = "address")
+    private String address;
 
-  @NotNull
-  @Column(name = "enabled")
-  private boolean enabled;
+    @Column(name = "mobile")
+    private String mobile;
 
-  @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-  @RestResource(exported = false)
-  private Set<Role> roles;
+    @Column(name = "landline")
+    private String landline;
 
-  @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "users")
-  private Collection<Course> courses = new ArrayList<>();
-  
-  @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
-  private Profile profile;
+    @NotNull
+    @Column(name = "account_non_expired")
+    private boolean accountNonExpired;
 
-  @OneToOne(cascade = {CascadeType.ALL})
-  private Inbox inbox;
+    @NotNull
+    @Column(name = "account_non_locked")
+    private boolean accountNonLocked;
 
-  @Lob
-  @Column(name = "about")
-  private String about;
+    @NotNull
+    @Column(name = "credentials_non_expired")
+    private boolean credentialsNonExpired;
 
-  @Lob
-  @Column(name = "avatar")
-  private String avatar;
+    @NotNull
+    @Column(name = "enabled")
+    private boolean enabled;
 
-  @Column(name="qq")
-  private String qq;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @RestResource(exported = false)
+    private Set<Role> roles;
 
-  @Column(name="qq_address")
-  private String qqAddress;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "users")
+    private Collection<Course> courses = new ArrayList<>();
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Profile profile;
+
+    @OneToOne(cascade = {CascadeType.ALL})
+    private Inbox inbox;
+
+    @Lob
+    @Column(name = "about")
+    private String about;
+
+    @Lob
+    @Column(name = "avatar")
+    private String avatar;
+
+    @Column(name = "qq")
+    private String qq;
+
+    @Column(name = "qq_address")
+    private String qqAddress;
 
     public String getQq() {
         return qq;
@@ -118,215 +116,215 @@ public class User extends AbstractDionysusPersistable implements UserDetails {
     }
 
     public String getAvatar() {
-    return avatar;
-  }
-
-  public void setAvatar(String avatar) {
-    this.avatar = avatar;
-  }
-
-  public String getAbout() {
-    return about;
-  }
-
-  public void setAbout(String about) {
-    this.about = about;
-  }
-
-  public User() {
-    this.accountNonExpired = true;
-    this.accountNonLocked = true;
-    this.credentialsNonExpired = true;
-    this.enabled = true;
-
-    this.roles = new HashSet<Role>();
-    this.inbox = new Inbox();
-    this.profile = new Profile(this);
-  }
-
-  public User(String username, String password) {
-    this();
-    this.username = username;
-    this.password = password;
-  }
-
-  @Override
-  public String getUsername() {
-    return username;
-  }
-
-  public Set<Role> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(Set<Role> roles) {
-    this.roles = roles;
-  }
-
-  public Collection<Course> getCourses() {
-	return courses;
-  }
-
-  public void setCourses(Collection<Course> courses) {
-	this.courses = courses;
-  }
-
-@Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    Collection<GrantedAuthority> authorities = new ArrayList<>();
-    Set<Role> userRoles = this.getRoles();
-
-    if (userRoles != null) {
-      for (Role role : userRoles) {
-        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.getName());
-        authorities.add(authority);
-      }
+        return avatar;
     }
-    return authorities;
-  }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public boolean grantAuthority(Role role) {
-    return roles.add(role);
-  }
-
-  public boolean revokeAuthority(Role role) {
-    return roles.remove(role);
-  }
-
-  @Override
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getEncryptedPassword() {
-    return encryptedPassword;
-  }
-
-  public void setEncryptedPassword(String encryptedPassword) {
-    this.encryptedPassword = encryptedPassword;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getGender() {
-	return gender;
-  }
-
-  public void setGender(String gender) {
-	this.gender = gender;
-  }
-
-  public String getAge() {
-	return age;
-  }
-
-  public void setAge(String age) {
-	this.age = age;
-  }
-
-  public String getAddress() {
-	return address;
-  }
-
-  public void setAddress(String address) {
-	this.address = address;
-  }
-
-  public String getMobile() {
-	return mobile;
-  }
-
-  public void setMobile(String mobile) {
-	this.mobile = mobile;
-  }
-
-  public String getLandline() {
-	return landline;
-  }
-
-  public void setLandline(String landline) {
-	this.landline = landline;
-  }
-
-  @Override
-  public boolean isAccountNonExpired() {
-    return accountNonExpired;
-  }
-
-  public void setAccountNonExpired(boolean accountNonExpired) {
-    this.accountNonExpired = accountNonExpired;
-  }
-
-  @Override
-  public boolean isAccountNonLocked() {
-    return accountNonLocked;
-  }
-
-  public void setAccountNonLocked(boolean accountNonLocked) {
-    this.accountNonLocked = accountNonLocked;
-  }
-
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return credentialsNonExpired;
-  }
-
-  public void setCredentialsNonExpired(boolean credentialsNonExpired) {
-    this.credentialsNonExpired = credentialsNonExpired;
-  }
-
-  @Override
-  public boolean isEnabled() {
-    return enabled;
-  }
-
-  public void setEnabled(boolean enabled) {
-    this.enabled = enabled;
-  }
-
-  public Profile getProfile() {
-    return profile;
-  }
-
-  public void setProfile(Profile profile) {
-    this.profile = profile;
-  }
-
-  public Inbox getInbox() {
-    return inbox;
-  }
-
-  public void setInbox(Inbox inbox) {
-    this.inbox = inbox;
-  }
-
-  @Override
-  public String toString() {
-    //satisfy UsernamepasswordAuthentionToken requirement.
-    return this.username;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj instanceof User) {
-      User other = (User) obj;
-      Long id = this.getId();
-      return (id != null) && id.equals(other.getId());
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
     }
-    return false;
-  }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public User() {
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+        this.enabled = true;
+
+        this.roles = new HashSet<Role>();
+        this.inbox = new Inbox();
+        this.profile = new Profile(this);
+    }
+
+    public User(String username, String password) {
+        this();
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Collection<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Collection<Course> courses) {
+        this.courses = courses;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        Set<Role> userRoles = this.getRoles();
+
+        if (userRoles != null) {
+            for (Role role : userRoles) {
+                SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.getName());
+                authorities.add(authority);
+            }
+        }
+        return authorities;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean grantAuthority(Role role) {
+        return roles.add(role);
+    }
+
+    public boolean revokeAuthority(Role role) {
+        return roles.remove(role);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getLandline() {
+        return landline;
+    }
+
+    public void setLandline(String landline) {
+        this.landline = landline;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public Inbox getInbox() {
+        return inbox;
+    }
+
+    public void setInbox(Inbox inbox) {
+        this.inbox = inbox;
+    }
+
+    @Override
+    public String toString() {
+        //satisfy UsernamepasswordAuthentionToken requirement.
+        return this.username;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof User) {
+            User other = (User) obj;
+            Long id = this.getId();
+            return (id != null) && id.equals(other.getId());
+        }
+        return false;
+    }
 }
