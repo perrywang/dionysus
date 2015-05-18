f3c8f224812cf4113b8560b914b352f6b5e48907
Perry Wang <perrywangsh@gmail.com>
2015-05-18 12:12:25 +0800

    PsychTestData will return array of PsychTestItem only containing ids

--- dionysus-rest/src/main/java/com/huixinpn/dionysus/dto/psychtest/PsychTestData.java
+++ dionysus-rest/src/main/java/com/huixinpn/dionysus/dto/psychtest/PsychTestData.java
@@ -1,9 +1,13 @@
 package com.huixinpn.dionysus.dto.psychtest;
 
 import com.huixinpn.dionysus.domain.psychtest.PsychTest;
+import com.huixinpn.dionysus.domain.psychtest.PsychTestItem;
 import com.huixinpn.dionysus.dto.EntityData;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Data
 @NoArgsConstructor
@@ -11,11 +15,15 @@ public class PsychTestData extends EntityData {
 
   private String title;
   private String description;
+  private Collection<Long> items = new ArrayList<>();
 
   public PsychTestData(PsychTest test) {
     super(test);
     this.title = test.getTitle();
     this.description = test.getDescription();
+    for(PsychTestItem item : test.getItems()){
+      items.add(item.getId());
+    }
   }
 
 }
