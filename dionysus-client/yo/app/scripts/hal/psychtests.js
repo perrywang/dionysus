9a18e5165172f108ebf6e49654ab87dd7422ee5a
Lu, Jun <luj1985@gmail.com>
2015-05-24 03:17:06 +0800

    add psychtest template

--- dionysus-client/yo/app/scripts/hal/psychtests.js
+++ dionysus-client/yo/app/scripts/hal/psychtests.js
@@ -1,71 +1,62 @@
 Dionysus.module('Domain', function(Domain, Dionysus, Backbone, Marionette, $) {
   'use strict';
 
+
+  var PsychTestQuestion = Backbone.RelationalHalResource.extend({
+    urlRoot: '/api/v1/psychtestquestions'
+  });
+
   var PsychTest = Backbone.RelationalHalResource.extend({
-
-  });
-
-
-  Domain.PsychTestQuestion = Backbone.Model.extend({
-  });
-
-  Domain.PsychTestQuestionCollection = Backbone.Collection.extend({
-    model: Domain.PsychTestQuestion,
-    url: '/api/v1/testitems',
-    parse: function (response) {
-      var embedded = response._embedded;
-      return embedded ? embedded.testitems : [];
+    urlRoot: '/api/v1/psychtests',
+    initialize: function() {
+      this.bind('change', this.updateCalculated, this);
     },
-    state: {
-      firstPage: 0
+    updateCalculated : function(){
+      var questions = this.embedded('questions') || [];
+      this.set('total', questions.length, { silent : true });
     },
-    queryParams: {
-      currentPage: 'page',
-      pageSize: 'size'
-    }
-  });
-
-  Domain.PsychTestQuestionOption = Backbone.Model.extend({
-    defaults: {
-      selected: false
-    }
-  });
-
-  Domain.PsychTestItemOptionCollection = Backbone.Collection.extend({
-    model: Domain.PsychTestQuestionOption
-  });
-  
-
-  var PsychTestSuiteResources = Backbone.RelationalHalResource.extend({
-    url: '/api/v1/psychtestsuites',
+    defaults : {
+      current : 1
+    },
     halEmbedded: {
-      psychtestsuites: {
+      questions: {
         type: Backbone.HasMany,
-        relatedModel: PsychTest
+        relatedModel: PsychTestQuestion
       }
     }
   });
 
-  Domain.PsychTest = Backbone.Model.extend({
-    defaults: {
-      'current': 0
-    },
-    urlRoot: '/api/v1/psychtests'
+  var PsychTestSuite = Backbone.RelationalHalResource.extend({});
+
+  var PsychTestSuiteCollection = Backbone.RelationalHalResource.extend({
+    url: '/api/v1/psychtestsuites',
+    halEmbedded: {
+      psychtestsuites: {
+        type: Backbone.HasMany,
+        relatedModel: PsychTestSuite
+      }
+    }
   });
 
-  Dionysus.reqres.setHandler('psychtestsuite:instance', function (id) {
-    var test = new Domain.PsychTest({id: id});
-    var defer = $.Deferred();
-    test.fetch().then(function () {
-      defer.resolve(test);
+  Dionysus.reqres.setHandler('psychtestsuite:instances', function() {
+    var resources = new PsychTestSuiteCollection(), defer = $.Deferred();
+    resources.fetch().then(function() {
+      defer.resolve(resources.embedded('psychtestsuites')); 
     });
     return defer.promise();
   });
 
-  Dionysus.reqres.setHandler('psychtestsuite:instances', function() {
-    var resources = new PsychTestSuiteResources(), defer = $.Deferred();
-    resources.fetch().then(function() { 
-      defer.resolve(resources.embedded('psychtestsuites')); 
+  Dionysus.reqres.setHandler('psychtest:instance', function (id) {
+    var test = PsychTest.findOrCreate({
+      id : id,
+      _links : {
+        self: {
+          href : '/api/v1/psychtests/' + id 
+        }
+      }
+    }), defer = $.Deferred();
+    test.fetch().then(function () {
+      defer.resolve(test);
     });
     return defer.promise();
   });
--- dionysus-client/yo/app/scripts/modules/test.js
+++ dionysus-client/yo/app/scripts/modules/test.js
@@ -1,100 +1,14 @@
 Dionysus.module('Test', function (Test, Dionysus, Backbone, Marionette, $, _) {
   'use strict';
 
-  var PsychTestItemOptionView = Marionette.ItemView.extend({
-    template: JST['templates/home/psychtests/option'],
-    initialize: function () {
-      this.model.on('change:selected', this.render, this);
-    },
-
-    ui: {
-      checkbox: 'input'
-    },
-
-    events: {
-      'click': 'selectOption'
-    },
-
-    selectOption: function () {
-      if (!this.model.get('selected')) {
-        this.trigger('option:changed', this.model.get('id'));
-
-      }
-    },
-
-    onRender: function () {
-      this.$el.toggleClass('test-item-option-selected', this.model.get('selected'));
-      this.ui.checkbox.prop('checked', this.model.get('selected'));
-    }
-  });
-
-  var PsychTestView = Marionette.LayoutView.extend({
-
-    template: JST['templates/home/psychtests/layout'],
-    className: 'ui segment',
-    regions: {
-      TestHeaderRegion: '#test-header',
-      TestItemRegion: '#test-item',
-      TestWizardRegion: '#test-wizard'
-    }
-
-  });
-
-  var HeaderView = Marionette.ItemView.extend({
-    template: JST["templates/home/psychtests/header"],
-    initialize: function () {
-      this.model.on('change:current', this.render, this);
-    }
-  });
-
-  var ItemView = Marionette.CompositeView.extend({
-    template: JST["templates/home/psychtests/question"],
-    childView: PsychTestItemOptionView,
-    childViewContainer: '#item-options'
-  });
-
-  var WizardView = Marionette.ItemView.extend({
-    template: JST["templates/home/psychtests/wizard"],
-    className: 'ui grid',
-    initialize: function () {
-      this.model.on('change:current', this.render, this);
-    },
-    events: {
-      'click #last': 'lastClicked',
-      'click #next': 'nextClicked'
-    },
-
-    ui: {
-      'lastBtn': '#last',
-      'nextBtn': '#next',
-      'submitBtb': '#submit'
-    },
-    onRender: function () {
-      var current = this.model.get('current');
-      var total = this.model.get('total');
-      if (current === 0) {
-        this.ui.lastBtn.addClass('disabled');
-      }
-      if (current === total - 1) {
-        this.ui.nextBtn.addClass('disabled');
-        this.ui.submitBtb.removeClass('disabled');
-      }
-
-    },
-
-    lastClicked: function () {
-      this.trigger('item:changed', -1);
-    },
-
-    nextClicked: function () {
-      this.trigger('item:changed', 1);
-    }
-  });
-
-
   var TestSuiteView = Marionette.ItemView.extend({
-    template: JST["templates/home/psychtests/suite"],
-    className : 'ui card'
+    template : JST["templates/home/psychtests/suite"],
+    className : 'ui card',
+    serializeData : function() {
+      var data = this.serializeModel(this.model);
+      data.tests = this.model.embedded('tests');
+      return data;
+    }
   });
 
   var TestSuiteCollectionView = Marionette.CollectionView.extend({
@@ -102,67 +16,31 @@ Dionysus.module('Test', function (Test, Dionysus, Backbone, Marionette, $, _) {
     className : 'ui list'
   });
 
+  var PsychTestQuestionView = Marionette.ItemView.extend({
+    template : JST['templates/home/psychtests/question']
+  });
+
+  var PsychTestView = Marionette.CompositeView.extend({
+    template : JST['templates/home/psychtests/workspace'],
+    childView : PsychTestQuestionView,
+    childViewContainer : '.question'
+  });
+
   var TestController = Marionette.Controller.extend({
     showTestSuites : function() {
-      var testsets = Dionysus.request('psychtestsuite:instances');
-      $.when(testsets).done(function(tests) {
-        var view = new TestSuiteCollectionView({ collection: tests });
-        Dionysus.mainRegion.show(view);
+      var fetching = Dionysus.request('psychtestsuite:instances');
+      $.when(fetching).done(function(suites) {
+        Dionysus.mainRegion.show(new TestSuiteCollectionView({ collection: suites }));
       });
     },
-    showTestSuite: function (id) {
-      var testFetching = Dionysus.request('psychtestsuite:instance', id);
-      $.when(testFetching).done(function (test) {
-        var total = test.get('items').length;
-        test.set('total', total);
-        var headerView = new HeaderView({
-          model: test
-        });
-        var current = test.get('current');
-        var item = new Dionysus.Entities.PsychTestItem({
-          description: test.get('items')[current].description
-        });
-
-        var options = new Dionysus.Entities.PsychTestItemOptionCollection(test.get('items')[current].options);
-
-        var itemView = new ItemView({
-          model: item,
-          collection: options
-        });
-
-        itemView.on('childview:option:changed', function (childview, selectedId) {
-          var current = test.get('current');
-          var currentItem = test.get('items')[current];
-          currentItem.options.map(function (option) {
-            option.selected = (option.id === selectedId);
-          });
-          itemView.collection.each(function (option) {
-            option.set('selected', option.get('id') === selectedId);
-          });
-        });
-
-        var wizardView = new WizardView({model: test});
-
-        wizardView.on('item:changed', function (step) {
-          var current = test.get('current') + step;
-          test.set('current', current);
-          var currentItem = test.get('items')[current];
-          itemView.model.set('description', currentItem.description);
-          itemView.collection = new Dionysus.Entities.PsychTestItemOptionCollection(test.get('items')[current].options);
-          itemView.render();
-        });
-
-
-        var testView = new PsychTestView();
-
-        testView.on('show', function () {
-          testView.TestHeaderRegion.show(headerView);
-          testView.TestItemRegion.show(itemView);
-          testView.TestWizardRegion.show(wizardView);
-
-        });
-
-        Dionysus.mainRegion.show(testView);
+    showTest: function (id) {
+      var fetching = Dionysus.request('psychtest:instance', id);
+      $.when(fetching).done(function (test) {
+        var questions = test.embedded('questions');
+        Dionysus.mainRegion.show(new PsychTestView({ 
+          model : test,
+          collection : questions
+        }));
       });
     }
   });
@@ -170,7 +48,7 @@ Dionysus.module('Test', function (Test, Dionysus, Backbone, Marionette, $, _) {
   Dionysus.addInitializer(function () {
     new Marionette.AppRouter({
       appRoutes: {
-        'psychtestsuites/:id(/)': 'showTestSuite',
+        'psychtest/:id(/)': 'showTest',
         'psychtestsuites(/)' : 'showTestSuites'
       },
       controller: new TestController()
