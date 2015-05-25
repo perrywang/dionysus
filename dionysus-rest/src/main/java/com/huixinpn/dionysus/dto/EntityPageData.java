fe9cf56310a5e1aef10e7b66f523048962da08d9
Perry Wang <perrywangsh@gmail.com>
2015-05-25 15:12:24 +0800

    paging add totalElements and totalPages fields

--- dionysus-rest/src/main/java/com/huixinpn/dionysus/dto/EntityPageData.java
+++ dionysus-rest/src/main/java/com/huixinpn/dionysus/dto/EntityPageData.java
@@ -13,11 +13,13 @@ import java.util.Collection;
 public class EntityPageData<T extends EntityData> {
   private static int defaultPageSize = 20;
   private Collection<T> content;
-  private long total;
+  private long totalElements;
+  private int totalPages;
   private int number;
   private int size;
   public EntityPageData(Page<? extends AbstractDionysusPersistable> page,Class<T> klass){
-    this.total = page.getTotalElements();
+    this.totalElements = page.getTotalElements();
+    this.totalPages = page.getTotalPages();
     this.number = page.getNumber();
     this.size = page.getSize();
     this.content = new EntityCollectionData<T>(page.getContent(),klass).toDTOCollection();
