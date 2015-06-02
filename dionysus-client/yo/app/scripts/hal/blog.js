a7715aa9742c9b5b8236601330f856f5c03782fb
Ryan Huang <hhao3000@gmail.com>
2015-06-03 00:21:11 +0800

    new plugin and log domain update

--- dionysus-client/yo/app/scripts/hal/blog.js
+++ dionysus-client/yo/app/scripts/hal/blog.js
@@ -29,24 +29,55 @@ Collection
 Reqeust Handler
 */
 
-Dionysus.reqres.setHandler('blog:item', function(id){
-	var blog = Blog.findOrCreate({id:id}), defer = $.Deferred();
-	blog.fetch().then(function(){
-		defer.resolve(blog);
+	Dionysus.reqres.setHandler('blog:item', function(id) {
+		var blog = Blog.findOrCreate({
+				id: id
+			}),
+			defer = $.Deferred();
+		blog.fetch().then(function() {
+			defer.resolve(blog);
+		});
+
+		return defer.promise();
 	});
 
-	return defer.promise();
-});
+	Dionysus.reqres.setHandler('blog:list', function(page, size) {
+		var blogResources = new BlogCollection(),
+			defer = $.Deferred();
+		var pageNumber = page ? page : 0;
 
-Dionysus.reqres.setHandler('blog:list', function(page, size){
-	var blogResources = new BlogCollection(), defer = $.Deferred();
-	var pageNumber = page? page: 0;
-
-	blogResources.fetch({data:{projection: 'blogsummary', page: pageNumber, size:size}}).then(function(){
-		defer.resolve(blogResources);
+		blogResources.fetch({
+			data: {
+				projection: 'blogsummary',
+				page: pageNumber,
+				size: size
+			}
+		}).then(function() {
+			defer.resolve(blogResources);
+		});
+		return defer.promise();
 	});
-	return defer.promise();
-});
+
+	Dionysus.reqres.setHandler('blog:user'),
+		function(userId, page, size) {
+			var blogResources = new BlogCollection({
+				url: "/api/v1/blogs/search/findByCreatedBy"
+			});
+			var defer = $.Deferred();
+
+			blogResources.fetch({
+				data: {
+					author: userId,
+					size: size,
+					page: page
+				}
+			}).then(function(blogs) {
+				defer.resolve(blogs)
+			});
+
+			return defer.promise();
+
+		});
 
 
 } );
