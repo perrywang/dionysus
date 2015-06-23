96ad430b290656f58fc2e343ede021c43a42f2e6
leancdf <leancdf@gmail.com>
2015-06-23 00:32:30 -0700

    add UI for appointment psychoprofile

---
+++ dionysus-client/yo/app/scripts/common/psychoprofile.js
@@ -0,0 +1,43 @@
+Dionysus.module('Entities', function(Entities, Dionysus, Backbone, Marionette, $) {
+  'use strict';
+
+  Entities.PsychoProfile = Backbone.Model.extend({
+    urlRoot: '/api/v1/profiles'
+  });
+
+  Entities.PsychoProfileCollection = Backbone.Collection.extend({
+    model: Entities.PsychoProfile,
+    url: '/api/v1/profiles',
+    parse: function(response) {
+      var embedded = response._embedded;
+      return embedded ? embedded.users : [];
+    },
+    state: {
+      firstPage: 0
+    },
+    queryParams: {
+      currentPage: 'page',
+      pageSize: 'size'
+    }
+  });
+
+  Dionysus.reqres.setHandler('psychoprofile:entities', function() {
+    var profiles = new Entities.PsychoProfileCollection();
+    var defer = $.Deferred();
+    profiles.fetch().then(function() {
+      defer.resolve(profiles);
+    });
+    return defer.promise();
+  });
+
+  Dionysus.reqres.setHandler('psychoprofile:entity', function(id) {
+    var profile = new Entities.PsychoProfile({id: id});
+    var defer = $.Deferred();
+    profile.fetch().then(function() {
+      defer.resolve(profile);
+    });
+    return defer.promise();
+  });
+
+});
+
