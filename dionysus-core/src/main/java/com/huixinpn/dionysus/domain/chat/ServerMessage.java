76ad6d8164e9faae2eb18a150ee394d787135f95
Ryan Huang <hhao3000@gmail.com>
2015-07-05 23:41:06 +0800

    server message

--- dionysus-core/src/main/java/com/huixinpn/dionysus/domain/chat/ServerMessage.java
+++ dionysus-core/src/main/java/com/huixinpn/dionysus/domain/chat/ServerMessage.java
@@ -1,8 +1,25 @@
 package com.huixinpn.dionysus.domain.chat;
 
+import lombok.Data;
+
 /**
  * Created by huanghao on 7/5/15.
  */
+@Data
 public class ServerMessage {
-    
+
+    private String content;
+    private String userName;
+    private Integer userId;
+    private String userAvatar;
+    private String timeStamp;
+
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public String getContent(){
+        return content;
+    }
 }
