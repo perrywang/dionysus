6f628b70d34305210a0f001d97edf0422a652238
Perry Wang <perrywangsh@gmail.com>
2015-07-07 10:40:36 +0800

    fix bug of PagingHelper

--- dionysus-webapp/src/main/java/com/huixinpn/dionysus/controller/util/PagingHelper.java
+++ dionysus-webapp/src/main/java/com/huixinpn/dionysus/controller/util/PagingHelper.java
@@ -10,7 +10,6 @@ public class PagingHelper {
   public static PageRequest getPageRequest(Integer page, Integer size) {
     int pageNumber = (page == null ? 0 : page);
     int pageSize = (size == null ? defaultPageSize : size);
-    defaultPageSize = pageSize;
     return new PageRequest(pageNumber, pageSize);
   }
 }
